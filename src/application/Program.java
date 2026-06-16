1ff55019558fbea85f8cbaaf30cbe4d846abb3fa
joaoVitor1009 <joaovitorsg1804@gmail.com>
2026-06-16 13:12:20 -0300

    First Class Position created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		Position position = new Position(3 , 5);
+		System.out.println(position);
 	}
 }
---
+++ src/boardGame/Position.java
@@ -0,0 +1,39 @@
+package boardGame;
+
+public class Position {
+	
+	private Integer column;
+	private Integer row;
+	
+	public Position() {
+	}
+
+	public Position(Integer column, Integer row) {
+	
+		this.column = column;
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [column= " + column + ", row= " + row + "]";
+	}
+	
+	
+}
